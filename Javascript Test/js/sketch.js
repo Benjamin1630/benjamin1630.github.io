69e6f285efa3049b73d3aaa47f3797090bf98edf
Red_Neb <benjamin1630@hotmail.com>
2023-10-09 20:27:49 +0200

    Fixed secret mode
    set display options to match the secret mode when activated

--- Javascript Test/js/sketch.js
+++ Javascript Test/js/sketch.js
@@ -2,8 +2,8 @@ let text1;
 let text2;
 let text1Default = ' ';
 let text2Default = ' ';
-let sliderCircleSize;
-let sliderCircleColour;
+let circleSize;
+let circleColour;
 let text1Box;
 let text1Colour;
 let text2Box;
@@ -22,11 +22,11 @@ function setup() {
     //create a canvas that's 400px in width and 400px in height
     createCanvas(displayWidth-25, displayHeight-150);
     //create a slider that changes Circle Size
-    sliderCircleSize = createSlider(0, 160, 160);
-    sliderCircleSize.position(400, 295);
+    circleSize = createSlider(0, 160, 160);
+    circleSize.position(400, 295);
     //create a colour picker for the colour of the circle
-    sliderCircleColour = createColorPicker('#ffffff');
-    sliderCircleColour.position(450,320);
+    circleColour = createColorPicker('#ffffff');
+    circleColour.position(450,320);
     //create first input for the text that populates the screen
     text1Box = createInput('');
     text1Box.position(900,305);
@@ -57,11 +57,11 @@ function draw() {
             //create a random coloured circle at the location of the mouse
             noStroke();
             if (rainbowMode === 0) {
-                fill(sliderCircleColour.value());
+                fill(circleColour.value());
             } else {
                 fill(random(256), random(256), random(256));
             }
-            circle(mouseX, mouseY, sliderCircleSize.value());
+            circle(mouseX, mouseY, circleSize.value());
         }
         //write some text on a random position within the box
         textSize(12);
@@ -70,6 +70,7 @@ function draw() {
         fill(text2Colour.value());
         text(text2, random(-50, displayWidth), random(-50, displayHeight));
         /*
+        //secret text
         if (text1 != ' ' || text2 != ' ') {
             showSecretTextChance = random(0, 1001);
             if (showSecretTextChance >= 1000) {
@@ -80,6 +81,7 @@ function draw() {
 }
 
 function keyPressed() {
+    //each new key press, store it, as well as previous 4 (total of 5)
     key5 = key4;
     key4 = key3;
     key3 = key2;
@@ -91,7 +93,7 @@ function keyPressed() {
         togglePause();
     } else if (keyCode === 13) { //upon keypress enter, set the text to show
         setText();
-    } else if (keyCode === 49) { //pressing 1 will toggle rainbow mode
+    } else if (keyCode === 97) { //pressing 1 will toggle rainbow mode
         secretRainbowMode();
     } else if (key5 == 78 && key4 == 79 && key3 == 82 && key2 == 65 && key1 == 72) { //this one does nothing
         norahMode();
@@ -99,19 +101,26 @@ function keyPressed() {
 }
 
 function reset() {
-    //this code will be executed when you click your left mouse button
+    //reset the canvas and disable rainbow mode
     clear();
     rainbowMode = 0;
-    //set the background color to a random RGB value up to 255
-    //background(random(256), random(256), random(256));
+    //set the background color value in slider
     background(bgColour.value());
-    //reset the pause bool on reset
+    //reset text
+    text1 = text1Default;
+    text1Box.value(text1Default);
+    text2 = text2Default;
+    text2Box.value(text1Default);
+    /*
+    //reset the pause bool when escape pressed
     if (pause === 1) {
         pause = 0;
     }
+    */
 }
 
 function togglePause() {
+    //toggle text & draw functionality
     if (pause === 0) {
         pause = 1;
     } else {
@@ -120,19 +129,21 @@ function togglePause() {
 }
 
 function setText() {
+    //if the textbox is empty, set the text to an empty character
     if (text1Box.value() != '') {
         text1 = text1Box.value();
     } else {
-        text1 = ' ';
+        text1 = text1Default;
     }
     if (text2Box.value() != '') {
         text2 = text2Box.value();
     } else {
-        text2 = ' ';
+        text2 = text2Default;
     }
 }
 
 function secretRainbowMode() {
+    //toggle the rainbow brush
     if (rainbowMode === 0) {
         rainbowMode = 1;
     } else {
@@ -142,8 +153,14 @@ function secretRainbowMode() {
 
 function norahMode() {
     clear();
-    background(255,0,255);
-    text1 = 'Norah';
-    text2 = 'Norah';
+    text1Box.value('Norah');
+    text1 = text1Box.value();
+    text2Box.value('Norah');
+    text2 = text2Box.value();
+    bgColour.value('#ff00ff')
+    background(bgColour.value());
+    circleColour.value('#ff0000');
+    circleSize.value(64);
 }
+
 //function mousePressed() {}
